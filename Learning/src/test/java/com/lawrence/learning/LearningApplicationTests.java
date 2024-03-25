acbf8b831fd81a968f6b1cced60af905a3945d25
Lawrence <haohsuan861104@gmail.com>
2024-03-25 11:49:22 +0800

    初始化專案與pom.xml配置

---
+++ Learning/src/test/java/com/lawrence/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawrence.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
